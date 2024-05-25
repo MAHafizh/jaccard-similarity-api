b236ce1cc8ba8fec1e774c32fe222d6f7a9f68b6
MAHafizh <hafizh12jr@gmail.com>
2024-05-25 17:59:28 +0700

    add retain cycle and delete routes

--- controller/index.js
+++ controller/index.js
@@ -1,11 +1,18 @@
+const { json } = require("express");
 const db = require("../database/firebaseConfig.js");
 
 const getDiagnosa = async (req, res) => {
-  let highSim = 0;
-  let mostSim = null;
+  let highSimilarity = 0;
+  let highSimilarityCaseId = null;
 
   try {
     const { gejala } = req.body;
+    const caseName = req.body.name;
+
+    if(gejala.length < 3) {
+      return res.status(400).json({msg: "Gejala kurang dari 3"});
+    } 
+
     const snapshotCase = await db.ref("data_kasus").once("value");
     const dataKasus = snapshotCase.val();
     const snapshotPenyakit = await db.ref("penyakit").once("value");
@@ -18,20 +25,24 @@ const getDiagnosa = async (req, res) => {
       const similarity = jaccardSimilarity(gejala, caseData.gejala);
       console.log(`Similarity for ${caseId}:`, similarity);
 
-      if (similarity > highSim) {
-        highSim = similarity;
-        mostSim = caseId;
+      if (similarity > highSimilarity) {
+        highSimilarity = similarity;
+        highSimilarityCaseId = caseId;
       }
     });
-    if (mostSim) {
-      const penyakitId = dataKasus[mostSim].penyakit;
+    if (highSimilarityCaseId) {
+      const penyakitId = dataKasus[highSimilarityCaseId].penyakit;
       const penyakitName = penyakit[penyakitId];
       const result = {
-        id: mostSim,
-        name: dataKasus[mostSim].name,
+        id: highSimilarityCaseId,
         penyakit: (`${penyakitName}(${penyakitId})`),
-        similarity: highSim.toFixed(3),
+        similarity: highSimilarity.toFixed(3),
       };
+
+      if (highSimilarity >= 0.75) {
+        await retainCase({gejala, caseName, penyakitId})
+      }
+
       res.json(result);
     } else {
       res.status(404).json({ msg: "Kasus yang mirip tidak ditemukan" });
@@ -42,6 +53,17 @@ const getDiagnosa = async (req, res) => {
   }
 };
 
+const deleteDataKasusbyId = async (req, res) => {
+  try {
+    const {caseId} = req.params;
+    await db.ref(`data_kasus/${caseId}`).remove();
+    res.status(200).json({msg: `Data dengan ID ${caseId} sudah dihapus`});
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+    console.error(error);
+  }
+}
+
 const jaccardSimilarity = (x, y) => {
   const irisan = x.filter((value) => y.includes(value)).length;
   const gabungan = new Set([...x, ...y]).size;
@@ -51,4 +73,34 @@ const jaccardSimilarity = (x, y) => {
   return result;
 };
 
-module.exports = { getDiagnosa };
+const retainCase = async (newCase) => {
+  try {
+    const snapshotCase = await db.ref("data_kasus").once("value");
+    const dataKasus = snapshotCase.val();
+
+    const isCaseExist = Object.values(dataKasus).some(
+      (existCase) => JSON.stringify(existCase.gejala.sort()) === JSON.stringify(newCase.gejala.sort())
+    );
+
+    if(isCaseExist) {
+      console.log("Kombinasi Gejala yang sama sudah ada dalam database");
+      return;
+    }
+
+    const caseId = Object.keys(dataKasus);
+    const lastId = caseId[caseId.length - 1];
+    const newIdnumber = parseInt(lastId.substring(2)) + 1;
+    const newid =  `BK${newIdnumber.toString().padStart(3, '0')}`;
+
+    await db.ref(`data_kasus/${newid}`).set({
+      gejala: newCase.gejala,
+      name: newCase.caseName,
+      penyakit: newCase.penyakitId
+    });
+    console.log("Kasus baru telah ditambah ke database");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = { getDiagnosa, deleteDataKasusbyId };
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {getDiagnosa} = require('../controller/index.js')
+const {getDiagnosa, deleteDataKasusbyId} = require('../controller/index.js')
 
 router.get('/', (req, res) => {
     res.send('API is running');
   });
 
 router.get('/diagnosa', getDiagnosa)
+router.delete('/delete-case/:caseId', deleteDataKasusbyId)
 
 module.exports = router;
